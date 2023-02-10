5b5e417aa8a20bb36aedd9150ce226318dfb31a5
bilel merhben <merhbenbilel91@gmail.com>
2023-02-10 19:22:43 +0100

    master

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BlogComponent } from './blog/blog.component';
+import { BlogDetailComponent } from './blog-detail/blog-detail.component';
+import { AddBlogComponent } from './add-blog/add-blog.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'blogs', component: BlogComponent },
+  { path: 'blogDetail', component: BlogDetailComponent },
+  { path: 'addBlog', component: AddBlogComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { MatCardModule } from '@angular/material/card';
+import { BlogComponent } from './blog/blog.component';
+import { AddBlogComponent } from './add-blog/add-blog.component';
+import { BlogDetailComponent } from './blog-detail/blog-detail.component';
+import { NavbarComponent } from './Shared/navbar/navbar.component';
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, BlogComponent, AddBlogComponent, BlogDetailComponent, NavbarComponent],
+  imports: [BrowserModule, AppRoutingModule, MatCardModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
